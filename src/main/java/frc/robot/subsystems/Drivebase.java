f4e8a351214b0dec992522ddca197e18e415d123
Raymonldle <variluoxx@gmail.com>
2023-02-11 16:28:23 -0800

    Revised code

---
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAlternateEncoder.Type;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivebase extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public Drivebase() {}
+
+  CANSparkMax m_leftMaster = new CANSparkMax(0,MotorType.kBrushless);
+  CANSparkMax m_leftSlave = new CANSparkMax(1,MotorType.kBrushless);
+  
+  CANSparkMax m_rightMaster = new CANSparkMax(2,MotorType.kBrushless);
+  CANSparkMax m_rightSlave = new CANSparkMax(3,MotorType.kBrushless);
+
+
+  DifferentialDrive m_differentialDrive = new DifferentialDrive(m_leftMaster,m_rightMotor);
+
+  public void arcadeDrive(double speed, double rotation){
+    m_differentialDrive.arcadeDrive(speed,rotation);
+  }
+
+  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
